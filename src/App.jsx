7abaa732ba3233eb7c873a0e83d510487fea1435
YoopaYenkels <leondaki@gmail.com>
2024-09-12 18:02:28 -0400

    implement add exercise to routine

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,48 @@ import './App.css'
 import WorkoutRoutine from './WorkoutRoutine'
 
 function App() {
-  const [routine, setRoutine] = useState([
+  const [exercises, setExercises] = useState([
     {id: 1, name: "Bench Press", sets: 3, reps: 5, weight: 165},
     {id: 2, name: "Dips", sets: 3, reps: 10, weight: "bodyweight"},
     {id: 3, name: "Shoulder Press", sets: 4, reps: 8, weight: 35},
     {id: 4, name: "Squats", sets: 3, reps: 12, weight: 45}
-  ])
+  ]);
+
+  const [newRowData, setNewRowData] = useState(
+    {id: -1, name: "", sets: "", reps: "", weight: ""}
+  );
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setNewRowData({...newRowData, [name] : value});
+    console.log(newRowData)
+  }
+
+  const addRow = () => {
+    const newRow = {
+      id: exercises.length + 1, 
+      name: newRowData.name, 
+      sets: newRowData.sets,
+      reps: newRowData.reps,
+      weight: newRowData.weight
+    }
+
+    setExercises([...exercises, newRow]);
+    setNewRowData({id: -1, name: "", sets: "", reps: "", weight: ""});
+  }
 
   return (
     <>
       <h1 class="text-blue-500 border py-10 my-10 text-4xl 
       border-blue-500 bg-white rounded-lg">
-        Welcome, Workout Warrior <span class="text-red-400">Leonidas</span>!
+        Welcome, Warrior <span class="text-red-400">Leonidas</span>!
         </h1>
 
-      <WorkoutRoutine routine={ routine }/>
+      <WorkoutRoutine 
+      exercises={ exercises } 
+      addRow={ addRow } 
+      onChange={ handleChange }
+      newRowData={ newRowData } />
     </>
   )
 }
--- src/WorkoutRoutine.jsx
+++ src/WorkoutRoutine.jsx
@@ -1,7 +1,6 @@
-function WorkoutRoutine({ routine }) {
+function WorkoutRoutine({ exercises, addRow, onChange, newRowData }) {
     return(
-    <>
-        <div class='shadow-md rounded-md mx-auto bg-white'>
+        <div class='shadow-md rounded-md mx-auto bg-white pb-8'>
             <h2 class='text-left p-8 font-bold text-3xl'>Daki's Workout</h2>
             <div class="pb-6 mx-8">
                 <table class="w-full border-collapse shadow-md bg-gray-100">
@@ -14,23 +13,39 @@ function WorkoutRoutine({ routine }) {
                         </tr>
                     </ thead>
                     <tbody>
-                        {routine.map((item) => (
-                                <tr key={item.id} class="even:bg-gray-200">
-                                    <td class="p-2">{item.name}</td>
-                                    <td class="p-2">{item.sets}</td>
-                                    <td class="p-2">{item.reps}</td>
-                                    <td class="p-2">{item.weight}</td>
+                        {exercises.map((exercise) => (
+                                <tr key={exercise.id} class="even:bg-gray-200">
+                                    <td class="p-2">{exercise.name}</td>
+                                    <td class="p-2">{exercise.sets}</td>
+                                    <td class="p-2">{exercise.reps}</td>
+                                    <td class="p-2">{exercise.weight}</td>
                                 </tr>
                                 )
                             )
                         }
+                        <tr>
+                            <td class='bg-gray-400 p-2'>
+                                <input onChange={onChange} name="name" value={newRowData.name} placeholder="Enter a name..."/>
+                            </td>
+                            <td class='bg-gray-400 p-2'>
+                                <input onChange={onChange} name="sets" value={newRowData.sets} placeholder="Enter set number..."/>
+                            </td>
+                            <td class='bg-gray-400 p-2'>
+                                <input onChange={onChange} name="reps" value={newRowData.reps} placeholder="Enter rep number..."/>
+                            </td>
+                            <td class='bg-gray-400 p-2'>
+                                <input onChange={onChange} name="weight" value={newRowData.weight} placeholder="Enter weight..."/>
+                            </td>
+                        </tr>
                     </tbody>
                         
                 </table>
             </div>
+            <button
+            class="border-2 border-green-500 hover:scale-105 p-2 px-4 rounded-lg
+            text-white bg-green-500 transition duration-150 ease-in-out"
+            onClick={() => addRow()}>Add Exercise</button>
         </ div>
-   
-    </>
   )
 }
 
